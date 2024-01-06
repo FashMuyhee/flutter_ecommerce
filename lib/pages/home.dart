ddf2d07b071e98743f785412a697dcf9a8a32d3a
Fash Muyhee <fashmuyidavid@gmail.com>
2024-01-06 21:18:43 +0100

    finish bottom bar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,12 +1,46 @@
+import '/pages/cart.dart';
+import '/pages/shop.dart';
+
+import '/widgets/bottom-nav-bar.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentTabIndex = 0;
+  final List<Widget> _pages = [const ShopPage(), const CartPage()];
+
+  void onTabChange(int index) {
+    setState(() {
+      currentTabIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey[300],
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: Builder(builder: (context) {
+          return IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () => {Scaffold.of(context).openDrawer()},
+          );
+        }),
+      ),
+      body: _pages[currentTabIndex],
+      drawer: const Drawer(
+        backgroundColor: Colors.black12,
+      ),
+      bottomNavigationBar: BottomNavBar(
+        onTabChange: (index) => onTabChange(index),
+      ),
     );
   }
 }
---
+++ lib/widgets/bottom-nav-bar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class BottomNavBar extends StatelessWidget {
+  void Function(int)? onTabChange;
+  BottomNavBar({super.key, required this.onTabChange});
+
+  @override
+  Widget build(BuildContext context) {
+    return GNav(
+        color: Colors.black,
+        activeColor: Colors.white,
+        mainAxisAlignment: MainAxisAlignment.center,
+        tabBackgroundColor: Colors.black,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        tabBorderRadius: 10,
+        tabMargin: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        gap: 10,
+        iconSize: 24,
+        onTabChange: (index) => onTabChange!(index),
+        tabs: const [
+          GButton(
+            icon: Icons.home,
+            text: 'Shop',
+          ),
+          GButton(icon: Icons.shopping_bag, text: 'Cart'),
+        ]);
+  }
+}
